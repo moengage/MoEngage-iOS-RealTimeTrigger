e1348c5e2390fe2ddb450c2ad55637af5860b38c
Soumya Ranjan Mahunt <133663939+msoumya-engg-sdk@users.noreply.github.com>
2024-05-28 10:33:10 +0530

    MOEN-30574: Added recommended SPM settings (#30)

--- Package.swift
+++ Package.swift
@@ -3,8 +3,11 @@
 
 import PackageDescription
 
+let version = "2.16.0"
+let checksum = "8c5d62b30843cdd5167a91709b5a8e23ce2dce5ff2ec1874d5657601c1365b47"
 let package = Package(
     name: "MoEngageRealTimeTrigger",
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "MoEngageRealTimeTrigger",
@@ -16,7 +19,8 @@ let package = Package(
         
     ],
     targets: [
-        .target(name: "MoEngageRealTimeTriggerSPM", dependencies: ["MoEngage-iOS-SDK","MoEngageRichNotification"], path: "Sources/..",exclude: ["MoEngageRealTimeTrigger.podspec", "README.md","./Frameworks/MoEngageRealTimeTrigger.xcframework", "LICENSE","CHANGELOG.md","Images/moe_logo_blue.png"]),
-        .binaryTarget(name: "MoEngageRealTimeTrigger",path: "./Frameworks/MoEngageRealTimeTrigger.xcframework")
-    ]
+        .target(name: "MoEngageRealTimeTriggerSPM", dependencies: ["MoEngage-iOS-SDK","MoEngageRichNotification"], path: "Sources/..",exclude: ["MoEngageRealTimeTrigger.podspec", "README.md", "LICENSE","CHANGELOG.md","Images/moe_logo_blue.png"]),
+        .binaryTarget(name: "MoEngageRealTimeTrigger", url: "https://github.com/moengage/MoEngage-iOS-RealTimeTrigger/releases/download/\(version)/MoEngageRealTimeTrigger.xcframework.zip", checksum: checksum)
+    ],
+    swiftLanguageVersions: [.v5]
 )
